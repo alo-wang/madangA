eefd6431a37d431c430f7364d2c2c4c3e24d821a
alo-wang <alo201011@gmail.com>
2025-04-02 12:17:01 +0900

    feat : 커넥션 프로바이더 - getConnection

--- madangA/src/madang/db/ConnectionProvider.java
+++ madangA/src/madang/db/ConnectionProvider.java
@@ -1,5 +1,22 @@
 package madang.db;
 
-public class ConnectionProvider {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class ConnectionProvider {
+	public static Connection getConnection(){
+		Connection conn = null;
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection(
+					  "jdbc:oracle:thin:@localhost:1521:XE",
+					  "c##madang",
+					  "madang");
+		} catch (Exception e) {
+			System.out.println("exception : " + e.getMessage());
+		}
+		
+		return conn;
+	}
 }
